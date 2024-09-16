f92766d7ab027dd4bcb63086ec0b8fb10bd00179
Andrii <andristwan28@gmail.com>
2024-09-16 18:04:18 +0700

    Add Resource Category

---
+++ app/Http/Resources/Api/CategoryResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources\Api;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'icon' => $this->icon,
+            'slug' => $this->slug,
+            'recipe_count' => $this->recipes->count,
+            // 'recipes' => RecipeResource::collection($this->whenLoaded('recipes')),
+        ];
+    }
+}
