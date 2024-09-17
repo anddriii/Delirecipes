b6f9dcd8ab226da6b97be16f4a42aac364d7e4ec
Andrii <andristwan28@gmail.com>
2024-09-17 23:54:33 +0700

    nothing

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -9,7 +9,6 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
     public function index(){
         $categories = Category::withCount('recipes')->get();
         return CategoryResource::collection($categories);
