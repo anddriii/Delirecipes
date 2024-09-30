2fdbe57a04a1812d201145299c1f18528d1aa8ec
Andrii <andristwan28@gmail.com>
2024-09-30 20:31:12 +0700

    Add API Recipe Author

---
+++ app/Http/Controllers/Api/RecipeAuthorController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\api\IngredientResource;
+use App\Http\Resources\Api\RecipeAuthorResource;
+use App\Models\RecipeAuthor;
+use Illuminate\Http\Request;
+
+class RecipeAuthorController extends Controller
+{
+    public function index() {
+        return RecipeAuthorResource::collection(RecipeAuthor::all());
+    }
+
+    public function show(RecipeAuthor $recipeAuthor){
+        return new RecipeAuthorResource($recipeAuthor);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\api\IngredientController;
+use App\Http\Controllers\Api\RecipeAuthorController;
 use App\Models\Ingredient;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,3 +17,6 @@ Route::apiResource('/categories', CategoryController::class);
 
 Route::get('/ingredient/{ingredient:id}', [IngredientController::class, 'show']);
 Route::apiResource('/ingredients', IngredientController::class);
+
+Route::get('/author/{recipeAuthor:id}', [RecipeAuthorController::class, 'show']);
+Route::apiResource('/authors', RecipeAuthorController::class);
